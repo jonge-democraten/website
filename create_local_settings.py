7744af474af7473df282912fb9146f87578ac2e9
Bart Romgens <bart.romgens@gmail.com>
2014-12-04 00:05:51 +0100

    Added script to create local settings.

---
+++ create_local_settings.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import codecs
+import os
+import random
+import shutil
+import string
+import tempfile
+
+LOCAL_SETTINGS_PATH = './website/local_settings.py'
+
+def main():
+    if os.path.exists(LOCAL_SETTINGS_PATH):
+        print(LOCAL_SETTINGS_PATH + ' already exists, please remove if manually if you indent to overwrite it.')
+    shutil.copyfile('./website/local_settings_example.py', LOCAL_SETTINGS_PATH)
+    secret_key_random = generate_random_secret_key()
+    replace(LOCAL_SETTINGS_PATH, "SECRET_KEY = ''", "SECRET_KEY = '" + secret_key_random + "'")
+
+def replace(source_file_path, pattern, substring):
+    fh, target_file_path = tempfile.mkstemp()
+
+    with codecs.open(target_file_path, 'w', 'utf-8') as target_file:
+        with codecs.open(source_file_path, 'r', 'utf-8') as source_file:
+            for line in source_file:
+                target_file.write(line.replace(pattern, substring))
+    os.remove(source_file_path)
+    shutil.move(target_file_path, source_file_path)
+
+def generate_random_secret_key():
+    # source: https://gist.github.com/mattseymour/9205591
+    # Get ascii Characters numbers and punctuation (minus quote characters as they could terminate string).
+    chars = ''.join([string.ascii_letters, string.digits, string.punctuation]).replace('\'', '').replace('"', '').replace('\\', '')
+    SECRET_KEY = ''.join([random.SystemRandom().choice(chars) for i in range(50)])    
+    return SECRET_KEY 
+
+if __name__ == "__main__":
+    main()
---
+++ website/local_settings_example.py
@@ -0,0 +1,21 @@
+DEBUG = True
+
+# SECURITY WARNING: Make this unique, and don't share it with anybody.
+SECRET_KEY = ''
+
+DATABASES = {
+    "default": {
+        # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.sqlite3",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "dev.db",
+        # Not used with sqlite3.
+        "USER": "",
+        # Not used with sqlite3.
+        "PASSWORD": "",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "",
+    }
+} 

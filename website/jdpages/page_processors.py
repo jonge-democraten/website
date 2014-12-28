24f284beab46462c224c7aeb1d95dc6b2f65b5ba
Bart Romgens <bart.romgens@gmail.com>
2014-12-29 00:23:01 +0100

    Initial page processor for jdpages.
    Adds the latests blogpost to a JDPage view context.
    To be shown in the right sidebar of jd pages.

---
+++ website/jdpages/page_processors.py
@@ -0,0 +1,19 @@
+import logging
+logger = logging.getLogger(__name__)
+
+from django.utils.html import strip_tags
+
+from mezzanine.blog.models import BlogCategory, BlogPost
+from mezzanine.core.models import CONTENT_STATUS_PUBLISHED
+from mezzanine.pages.page_processors import processor_for
+
+from .models import JDPage
+
+
+@processor_for(JDPage)
+def jdpage(request, page):
+    logger.warning(page.title)
+    sidebar_blog_category = BlogCategory.objects.get(id=1)
+    sidebar_blog_posts = BlogPost.objects.all().filter(categories=sidebar_blog_category).filter(status=CONTENT_STATUS_PUBLISHED)
+    logger.warning(sidebar_blog_posts)
+    return {"sidebar_blog_post": sidebar_blog_posts.last()}

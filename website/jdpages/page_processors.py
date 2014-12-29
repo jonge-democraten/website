7b1a548657ce64403ef44806b1f9b60ae9d3bc35
Bart Römgens <bart.romgens@gmail.com>
2014-12-30 00:55:36 +0100

    Added politiek blogposts to homepage.

--- website/jdpages/page_processors.py
+++ website/jdpages/page_processors.py
@@ -14,8 +14,9 @@ class BlogPostInfo():
     def __init__(self, blogpost):
         self.title = blogpost.title
         self.author = blogpost.user
-        self.content = strip_tags(blogpost.content)
+        self.date = blogpost.publish_date
         self.url = blogpost.get_absolute_url()
+        self.content = strip_tags(blogpost.content)
 
 
 @processor_for(JDPage)
@@ -24,5 +25,17 @@ def add_sidebar_blog_info(request, page):
     sidebar_blog_category = BlogCategory.objects.get(id=1)
     sidebar_blog_posts = BlogPost.objects.all().filter(categories=sidebar_blog_category).filter(status=CONTENT_STATUS_PUBLISHED)
     sidebar_blogpost = sidebar_blog_posts.last()
+    if not sidebar_blogpost:
+        return
     blogpost_info = BlogPostInfo(sidebar_blogpost)
     return {"sidebar_blogpost": blogpost_info}
+
+
+@processor_for(JDHomePage)
+def add_politiek_blogposts(request, page):
+    politiek_blog_category = BlogCategory.objects.get(title="Politiek")
+    politiek_blog_posts = BlogPost.objects.all().filter(categories=politiek_blog_category).filter(status=CONTENT_STATUS_PUBLISHED)[:3]
+    politiek_blogposts_info = []
+    for post in politiek_blog_posts:
+        politiek_blogposts_info.append(BlogPostInfo(post))
+    return {"politiek_blogposts_info": politiek_blogposts_info}

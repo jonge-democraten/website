aa14cdaed6faa98c3a4f43346a7030e43999bd5c
Bart Römgens <bart.romgens@gmail.com>
2015-01-02 18:16:25 +0100

    Added a generic page column items to JDHomePage.
    These items can represent any model and contain the site they are active for.
    Examples are a BlogCategory preview and an Events preview on the current homepage.

--- website/jdpages/models.py
+++ website/jdpages/models.py
@@ -5,10 +5,14 @@ Models that extend mezzanine Pages and add JD specific data.
 import logging
 logger = logging.getLogger(__name__)
 
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-from mezzanine.core.models import RichText
+from mezzanine.blog.models import BlogCategory
+from mezzanine.core.models import RichText, Displayable
 from mezzanine.pages.models import Page
+from mezzanine.utils.sites import Site
 
 
 class JDContentBase(models.Model):
@@ -23,13 +27,34 @@ class JDContentBase(models.Model):
         abstract = True
 
 
+class JDColumnItem(models.Model):
+    content_type = models.ForeignKey(ContentType, blank=True, null=True)
+    object_id = models.PositiveIntegerField(null=True, verbose_name='related object id')
+    content_object = GenericForeignKey('content_type', 'object_id')
+    site = models.ForeignKey(Site, default=1, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.content_object) + ' (' + str(self.content_type) + ')'
+    
+    class Meta:
+        verbose_name = u'JD Column Item'
+
+
 class JDPage(Page, RichText, JDContentBase):
     """
-    Page model for general richtext pages
+    Page model for general richtext pages.
     """
+    
+    class Meta:
+        verbose_name = u'JD Page'
 
 
 class JDHomePage(Page, RichText, JDContentBase):
     """
-    Page model for the JD homepage (jd.nl)
+    Page model for the site homepage
     """
+    column_items_left = models.ManyToManyField(JDColumnItem, blank=True, null=True, related_name="column_items_left")
+    column_items_right = models.ManyToManyField(JDColumnItem, blank=True, null=True, related_name="column_items_right")
+
+    class Meta:
+        verbose_name = u'JD Homepage'

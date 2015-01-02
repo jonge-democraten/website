aa4b3f982713d79f97fb6d51340142a58093d050
Bart Römgens <bart.romgens@gmail.com>
2015-01-02 18:54:07 +0100

    Automatically create a column item for each BlogCategory that is created. These items can be selected later to be shown in on the homepage columns.

--- website/jdpages/signals.py
+++ website/jdpages/signals.py
@@ -2,10 +2,30 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from mezzanine.blog.models import BlogCategory
+
+from website.jdpages.models import JDColumnItem
+
+
 @receiver(post_save)
 def post_save_callback(sender, instance, created, **kwargs):
     """
     Called after a model instance is saved.
-    """
+    Created related database objects for some 
     
-    return # TODO BR: implement
+    Arguments:
+    sender -- the model class
+    instance -- the actual instance being saved
+    created -- a boolean; True if a new record was created
+    """
+    if not created:
+        return
+    if (sender == BlogCategory):
+        if JDColumnItem.objects.filter(object_id=instance.id, content_type=ContentType.objects.get_for_model(sender)):
+            return
+        column_item = JDColumnItem()
+        column_item.content_type = ContentType.objects.get_for_model(sender)
+        column_item.object_id = instance.id
+        column_item.site = instance.site
+        column_item.save()
+        return

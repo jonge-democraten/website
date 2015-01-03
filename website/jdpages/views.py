d77d98de3fc5f7a92a28c1a743b2e00b41742875
Bart Römgens <bart.romgens@gmail.com>
2015-01-03 19:40:25 +0100

    Fixed function name and improved docstring.

--- website/jdpages/views.py
+++ website/jdpages/views.py
@@ -1,9 +1,19 @@
+import logging
+logger = logging.getLogger(__name__)
+
 from django.shortcuts import render
 
+from mezzanine.blog.models import BlogCategory
 
-def get_elements_with_items(elements):
+
+def get_elements_with_items(element_widgets):
+    """ 
+    Adds the element items to this element 
+    Contains a ContentType type switch which determines 
+    elements --- a list of ColumnElementWidgets
+    """
     elements_new = []
-    for element in elements:
+    for element in element_widgets:
         if element.content_type.model_class() == BlogCategory:
-            elements_new.append(BlogCategoryElement.get_original_element(element))
+            elements_new.append(BlogCategoryElement.get_element_with_items(element))
     return elements_new

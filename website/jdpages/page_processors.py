ad4eacbb7c98dd3903a7c77c81eb15e1b4184447
Bart Römgens <bart.romgens@gmail.com>
2015-01-03 19:33:52 +0100

    Moved get_elements_with_items to jdpages.view.
    Is a view function.

--- website/jdpages/page_processors.py
+++ website/jdpages/page_processors.py
@@ -8,27 +8,19 @@ logger = logging.getLogger(__name__)
 
 from django.conf import settings
 
-
 from mezzanine.blog.models import BlogCategory, BlogPost
 from mezzanine.pages.page_processors import processor_for
 
 from website.jdpages.models import JDPage, JDHomePage, ColumnElementWidget, BlogCategoryElement
 from website.utils.containers import BlogPostItem
+from website.jdpages.views import get_elements_with_items
 
 
 @processor_for(JDHomePage)
 def add_column_elements(request, page):
     elements_left = page.jdhomepage.column_elements_left.all()
-    elements_org_left = get_original_elements(elements_left)
+    elements_org_left = get_elements_with_items(elements_left)
     elements_right= page.jdhomepage.column_elements_right.all()
-    elements_org_right = get_original_elements(elements_right)
+    elements_org_right = get_elements_with_items(elements_right)
     return {"column_elements_left": elements_org_left,
             "column_elements_right": elements_org_right}
-
-
-def get_original_elements(elements):
-    elements_new = []
-    for element in elements:
-        if element.content_type.model_class() == BlogCategory:
-            elements_new.append(BlogCategoryElement.get_original_element(element))
-    return elements_new
--- website/jdpages/views.py
+++ website/jdpages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_elements_with_items(elements):
+    elements_new = []
+    for element in elements:
+        if element.content_type.model_class() == BlogCategory:
+            elements_new.append(BlogCategoryElement.get_original_element(element))
+    return elements_new

f8dc17d76c9634ed3d8e905d38f07f7e2669bed2
Lucas van Dijk <info@return1.net>
2015-01-11 16:33:27 +0100

    Added model for repeating events.
    The actual implentation may change, but this model
    will be used to store the metadata.

--- website/jdevents/models.py
+++ website/jdevents/models.py
@@ -2,6 +2,20 @@ from django.db import models
 
 from mezzanine.core.models import Displayable, RichText
 
+
+class RepeatType(models.Model):
+    DAILY = 'daily'
+    WEEKLY = 'weekly',
+    MONTHLY = 'monthly'
+
+    REPEAT_CHOICES = (
+        (DAILY, 'REPEAT_DAILY'),
+        (WEEKLY, 'REPEAT_WEEKLY'),
+        (MONTHLY, 'REPEAT_MONTHLY')
+    )
+
+    repeat_type = models.CharField(max_length=10, choices=REPEAT_CHOICES)
+
 class Event(Displayable, RichText):
     """
         Main object for each event.
@@ -18,4 +32,5 @@ class Event(Displayable, RichText):
 
     event_date = models.DateTimeField()
     event_end = models.DateTimeField()
+    repeat = models.ForeignKey(RepeatType, default=None, blank=True)
 

c3a4d790acbb6a0ff8657da105f1c049bf64b209
Lucas van Dijk <info@return1.net>
2015-01-11 16:30:24 +0100

    Update event model to use the mezzanine models.
    Use the core abstract models from mezzanine, which adds several
    standard fields, and integrates with the mezzanine search.

--- website/jdevents/models.py
+++ website/jdevents/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
-class Event(models.Model):
+from mezzanine.core.models import Displayable, RichText
+
+class Event(Displayable, RichText):
     """
-        Main object for each event
+        Main object for each event.
+
+        Derives from Displayable, which by default
+
+        - it is related to a certain Site object
+        - it has a title and a slug
+        - it has SEO metadata
+        - it gets automated timestamps when the object is updated
+
+        Besides that, it derives from RichText, which provides a WYSIWYG field.
     """
 
-    title = models.CharField(max_length=200)
+    event_date = models.DateTimeField()
+    event_end = models.DateTimeField()
 

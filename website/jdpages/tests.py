f460bd2b22d24b2e367fdec2c1ce310196a0243f
Bart Römgens <bart.romgens@gmail.com>
2015-01-13 00:09:01 +0100

    Added ColumnElement unit test.
    Tests that a ColumnElement is created for each BlogCategory, and contains a reference
    to this category.

--- website/jdpages/tests.py
+++ website/jdpages/tests.py
@@ -1,3 +1,23 @@
+import logging
+logger = logging.getLogger(__name__)
+
 from django.test import TestCase
 
-# Create your tests here.
+from mezzanine.blog.models import BlogCategory
+
+from .models import ColumnElement
+
+
+class ColumnElementTest(TestCase):
+    """ ColumnElement test case. """
+
+    def test_auto_create(self):
+        """
+        Tests whether a ColumnElement is automatically created
+        when a BlogCategory is created, and that the elements
+        has a reference to the new blog category.
+        """
+        category = BlogCategory.objects.create(title="Test Blog")
+        self.assertEqual(ColumnElement.objects.count(), 1)
+        element = ColumnElement.objects.get(id=1)
+        self.assertEqual(element.get_object(), category)

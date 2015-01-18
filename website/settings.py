dc564d36d24afed3b5521dd1a6c1553c84e9e0ab
Pieter <@>
2015-01-18 13:42:26 +0100

    Revert "Change whitelist to work with department map"
    This reverts commit 7ed3c28ba644e31e1e9602e7a8dcfc461c28ed01.
    
    Conflicts:
    	website/settings.py

--- website/settings.py
+++ website/settings.py
@@ -496,7 +496,7 @@ RICHTEXT_SCRIPT_TAG_WHITELIST = (
     '<script type="text/javascript" src="http://d3js.org/queue.v1.min.js"></script>',
     '<script type="text/javascript" src="http://d3js.org/d3.geo.projection.v0.min.js"></script>',
     '<script type="text/javascript" src="http://d3js.org/topojson.v0.min.js"></script>',
-    '<script src="/static/js/render.js" type="text/javascript"></script>',
+    '<script type="text/javascript" src="http://jongedemocraten.nl/templates/jd/javascript/kaart/render.js"></script>',
 )
 
 ##########################

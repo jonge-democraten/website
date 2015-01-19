d8e5f075ab0111391d5c09e824843fb65d45fabe
Bart Romgens <bart.romgens@gmail.com>
2015-01-19 22:52:51 +0100

    Added two tests for sidebar elements.

--- website/jdpages/tests.py
+++ website/jdpages/tests.py
@@ -5,7 +5,9 @@ from django.test import TestCase
 
 from mezzanine.blog.models import BlogCategory
 
-from .models import ColumnElement
+from website.jdpages.models import ColumnElement
+from website.jdpages.models import SidebarElement
+from website.jdpages.models import SocialMediaButtonGroup
 
 
 class TestColumnElement(TestCase):
@@ -14,8 +16,7 @@ class TestColumnElement(TestCase):
     def test_auto_create(self):
         """
         Tests whether a ColumnElement is automatically created
-        when a BlogCategory is created, and that the elements
-        has a reference to the new blog category.
+        when a BlogCategory is created, and that the element has a to it.
         """
         category = BlogCategory.objects.create(title="Test Blog")
         self.assertEqual(ColumnElement.objects.count(), 1)
@@ -34,3 +35,31 @@ class TestColumnElement(TestCase):
         category.delete()
         self.assertEqual(BlogCategory.objects.count(), 0)
         self.assertEqual(ColumnElement.objects.count(), 0)
+
+
+class TestSidebarElement(TestCase):
+    """ SidebarElement test case. """
+
+    def test_auto_create(self):
+        """
+        Tests whether a SidebarElement is automatically created
+        when a SocialMediaButtonGroup is created, 
+        and that the element has a reference to it.
+        """
+        group = SocialMediaButtonGroup.objects.create(name="Test Button Group")
+        self.assertEqual(SidebarElement.objects.count(), 1)
+        element = SidebarElement.objects.get(id=1)
+        self.assertEqual(element.get_object(), group)
+
+    def test_auto_delete(self):
+        """
+        Tests whether the related SidebarElement is automatically deleted
+        when a SocialMediaButtonGroup is deleted.
+        """
+        group = SocialMediaButtonGroup.objects.create(name="Test Button Group")
+        self.assertEqual(SidebarElement.objects.count(), 1)
+        element = SidebarElement.objects.get(id=1)
+        self.assertEqual(element.get_object(), group)
+        group.delete()
+        self.assertEqual(SocialMediaButtonGroup.objects.count(), 0)
+        self.assertEqual(SidebarElement.objects.count(), 0)

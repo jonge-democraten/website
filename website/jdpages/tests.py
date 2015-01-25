497936a615cd447ae933ca09335e16c1ffc7b0cb
Bart Römgens <bart.romgens@gmail.com>
2015-01-25 22:35:43 +0100

    Added sidebar unit tests.

--- website/jdpages/tests.py
+++ website/jdpages/tests.py
@@ -2,10 +2,15 @@ import logging
 logger = logging.getLogger(__name__)
 
 from django.test import TestCase
+from django.test import Client
 
 from mezzanine.blog.models import BlogCategory
 
 from website.jdpages.models import ColumnElement
+from website.jdpages.models import Sidebar
+from website.jdpages.views import BlogCategorySidebarItem
+from website.jdpages.views import TwitterSidebarItem
+from website.jdpages.views import SocialMediaButtonGroupItem
 
 
 class TestColumnElement(TestCase):
@@ -33,3 +38,34 @@ class TestColumnElement(TestCase):
         category.delete()
         self.assertEqual(BlogCategory.objects.count(), 0)
         self.assertEqual(ColumnElement.objects.count(), 0)
+
+
+class TestSidebar(TestCase):
+    """ Tests the sidebar and its widgets. """
+    fixtures = ['test_sidebar.json']
+
+    def setUp(self):
+        self.client = Client()
+
+    def login(self):
+        """ Login as admin. """
+        return self.client.post('/admin/login/?next=/admin/', {'username': 'admin', 'password': 'admin'}, follow=True)
+
+    def test_edit_sidebar_admin_view(self):
+        """ Tests whether the change sidebar admin view response is OK (200). """
+        sidebars = Sidebar.objects.all()
+        self.assertEquals(sidebars.count(), 1)
+        sidebar = sidebars[0]
+        response = self.login()
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/admin/jdpages/sidebar/' + str(sidebar.id) + '/', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_items_sidebar(self):
+        """ Tests whether the sidebar view items exist in the homepage context. """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        items = response.context['sidebar_items']
+        self.assertTrue(type(items[0]) == BlogCategorySidebarItem)
+        self.assertTrue(type(items[1]) == TwitterSidebarItem)
+        self.assertTrue(type(items[2]) == SocialMediaButtonGroupItem)

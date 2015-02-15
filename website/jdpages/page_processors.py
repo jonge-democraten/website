8fef57a1d194fc5c783c3aadc68e434e737e253e
Bart Römgens <bart.romgens@gmail.com>
2015-02-15 19:58:54 +0100

    Add page processor that adds the header image.

--- website/jdpages/page_processors.py
+++ website/jdpages/page_processors.py
@@ -7,10 +7,32 @@ import logging
 logger = logging.getLogger(__name__)
 
 from mezzanine.pages.page_processors import processor_for
+from mezzanine.pages.models import RichTextPage
 
 from website.jdpages.models import HomePage, ColumnElementWidget
 from website.jdpages.models import HorizontalPosition
+from website.jdpages.models import PageHeaderSettingsWidget, PageHeaderImageWidget
 from website.jdpages.views import create_column_items
+
+@processor_for(RichTextPage)
+def add_header_images(request, page):
+    page_header_settings = PageHeaderSettingsWidget.objects.get(page=page)
+    page_header = []
+    if page_header_settings.type == PageHeaderSettingsWidget.SINGLE:
+        page_header = PageHeaderImageWidget.objects.filter(page=page)[0]
+    if page_header_settings.type == PageHeaderSettingsWidget.PARENT:
+        parent = page.parent
+        if parent:
+            page_header = PageHeaderImageWidget.objects.filter(page=parent)[0]
+        else:
+            homepage = HomePage.objects.all()[0]
+            page_header = PageHeaderImageWidget.objects.filter(page=homepage)[0]
+    if page_header_settings.type == PageHeaderSettingsWidget.NONE:
+        page_header = []
+    if page_header_settings.type == PageHeaderSettingsWidget.RANDOM:
+        page_headers = PageHeaderImageWidget.objects.filter(page=page)
+        page_headers
+    return {"page_header": page_header}
 
 
 @processor_for(HomePage)

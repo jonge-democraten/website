b271684bdccd1cca0f4686559ab4b219b7c44997
Lucas van Dijk <info@return1.net>
2015-04-30 22:26:11 +0200

    Fix loading of event details map

--- website/static/js/events.js
+++ website/static/js/events.js
@@ -2,14 +2,12 @@ jQuery(function($) {
     $(document).ready(function() {
         // Search for lat-long coordinates of the given location
         $.ajax({
-            url: location.protocol + "//maps.google.com/maps/api/geocode/json",
+            url: "https://maps.google.com/maps/api/geocode/json",
             dataType: "json",
+            crossDomain: true,
             data: {
                 "address": $('#occurrence-location').text(),
                 "sensor": "false"
-            },
-            xhrFields: {
-                withCredentials: true
             },
             success: function(data) {
                 console.log(data);
@@ -26,8 +24,14 @@ jQuery(function($) {
                 });
 
                 // Show a map on the event detail page
-                var map = L.map('event-map').setView([data[0].lat, data[0].lon],
-                    15);
+                var map = L.map('event-map').setView(
+                    [
+                        data.results[0].geometry.location.lat,
+                        data.results[0].geometry.location.lng
+                    ],
+                    15
+                );
+
                 L.tileLayer(location.protocol + '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                     attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
                     maxZoom: 18
@@ -35,7 +39,7 @@ jQuery(function($) {
 
                 var marker = L.marker([
                     data.results[0].geometry.location.lat, 
-                    data.results[0].geometry.location.lon
+                    data.results[0].geometry.location.lng
                 ]).addTo(map);
             },
         });

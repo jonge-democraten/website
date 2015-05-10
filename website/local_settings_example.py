b084767f31ebd2ea41c0f11b1c8531205d281fbc
Bart Römgens <bart.romgens@gmail.com>
2015-05-10 14:10:17 +0200

    Add demo data hosts to local settings example
    Makes the demo data site also work out-of-the-box when settings.DEBUG = False.

--- website/local_settings_example.py
+++ website/local_settings_example.py
@@ -6,7 +6,7 @@ SECRET_KEY = ''
 
 TIME_ZONE = "Europe/Amsterdam"
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'jd.local', 'lh.jd.local', 'ams.jd.local']
 
 DATABASES = {
     "default": {

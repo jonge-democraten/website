ec11e1a14ffb3d0efbbfac40eb60ed0a6728bbc6
Bart Römgens <bart.romgens@gmail.com>
2017-12-09 12:17:49 +0100

    Fix static url example setting

--- website/local_settings_example.py
+++ website/local_settings_example.py
@@ -41,9 +41,9 @@ LANGUAGE_CODE = "nl-NL"
 
 # STATIC and MEDIA root and url need to be set in production!
 # STATIC_ROOT = "<path-to-project>/static"
-# STATIC URL = /static
+# STATIC_URL = /static/
 # MEDIA_ROOT = "<path-to-project>/media"
-# MEDIA_URL = /media
+# MEDIA_URL = /media/
 
 ###################
 # JANEUS SETTINGS #
